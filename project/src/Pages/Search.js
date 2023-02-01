d5643ac2b71b07113d3454bb9aea260b39bf7c1d
judeshenal <2000daredevil@gmail.com>
2023-02-01 11:22:45 +0530

    Committing the project again

---
+++ project/src/Pages/Search.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './search.css'
+
+export default function Search() {
+
+
+    return (
+    <div>
+        <div classname="topic">
+            <span>All</span>
+            <span>Unread</span>
+            <span>Filter Notification</span>
+            <button onClick={"car"}></button>
+        </div>
+    </div>
+  )
+}
